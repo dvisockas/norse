a63e76ffd116bd1a32e8521a3d741d9bde43229c
Danielius <danieliusvisockas@gmail.com>
2021-05-13 15:55:24 +0300

    Add dynamic norms

--- norse/models/dynamic_model.py
+++ norse/models/dynamic_model.py
@@ -3,31 +3,32 @@ from torch import nn
 
 UPSAMPLE_TYPES  = ['transpose', 'conv']
 ATTENTION_TYPES = ['SAP', 'ASP']
-SKIP_OP_TYPES   = ['add', 'cat']
+SKIP_OP_TYPES   = ['add', 'cat', 'none']
+NORM_TYPES      = ['batch', 'layer']
 
 class Autoencoder(nn.Module):
     def __init__(
         self,
-        pay_attention = False,
-        encoder_type  = 'SAP',
+        pay_attention = 0,
         depth         = 6,
         channels_in   = 1,
         channels_out  = 1,
         growth_factor = 16,
         kernel_size   = 16,
         upsample_type = 'transpose',
-        skip_op       = 'add'
+        skip_op       = 'add',
+        norm          = 'batch'
     ):
         self.attn = pay_attention
         self.depth = depth
         self.growth_factor = growth_factor
-        self.encoder_type = encoder_type
-        assert(encoder_type in ATTENTION_TYPES)
 
         self.kernel_size = kernel_size
+        self.norm = norm
+        assert(norm in NORM_TYPES)
+        
         self.upsample_type = upsample_type
-        if pay_attention:
-            assert(upsample_type in UPSAMPLE_TYPES)
+        assert(upsample_type in UPSAMPLE_TYPES)
 
         self.padding_mode = 'reflect'
         
@@ -40,27 +41,31 @@ class Autoencoder(nn.Module):
     def bootstrap(self):
         self.encoder = nn.ModuleList()
         self.decoder = nn.ModuleList()
+        self.attention = nn.ModuleList()
 
         for index in range(self.depth):
             encoder_ch_in = max(self.growth_factor * (index), 1)
             encoder_ch_out = self.growth_factor * (index + 1)
-
+            norm = nn.BatchNorm1d if self.norm == 'batch' else nn.LayerNorm
+            
             encode = []
             encode += [
                 nn.Conv1d(
                     encoder_ch_in, encoder_ch_out, self.kernel_size, stride=2, padding=15, padding_mode=self.padding_mode, bias=False,
                 ),
-                nn.BatchNorm1d(encoder_ch_out),
+                norm(encoder_ch_out),
                 nn.GELU(),
             ]
 
             self.encoder.append(nn.Sequential(*encode))
 
-            channel_multiplier = 2 if self.skip_op == 'cat' else 1
+            channel_multiplier = 2 if (self.skip_op == 'cat' and index != 0) else 1
             
             growth = self.growth_factor * channel_multiplier
+            
             decoder_ch_in = growth * (self.depth-index)
-            decoder_ch_out = (growth * (self.depth-index-1)) #// 2
+            decoder_ch_out = (growth * (self.depth-index-1))
+            if self.skip_op == 'cat' and index != 0: decoder_ch_out = decoder_ch_out // 2
             decoder_ch_out = max(decoder_ch_out, 1)
 
             stride = self.kernel_size if decoder_ch_out == 0 else 1
@@ -82,27 +87,31 @@ class Autoencoder(nn.Module):
 
             activation = nn.Tanh() if decoder_ch_out == 1 else nn.GELU()
 
-            #activation = nn.GELU()
             if decoder_ch_out > 1:
-                decode.append(nn.BatchNorm1d(decoder_ch_out))
+                decode.append(norm(decoder_ch_out))
                 decode.append(activation)
 
             self.decoder.append(nn.Sequential(*decode))
-
-
-        self.attention = nn.Sequential(
-            nn.Conv1d(encoder_ch_out, 128, kernel_size=1, bias=False),
-            nn.GELU(),
-            nn.BatchNorm1d(128),
-            nn.Conv1d(128, encoder_ch_out, kernel_size=1),
-            nn.Softmax(dim=1),
-            )
+        
+        attn_dims = self.growth_factor * self.depth
+        
+        for i in range(self.attn):
+            one_filter = [
+                nn.Conv1d(attn_dims, attn_dims, 1),
+                nn.Conv1d(attn_dims, attn_dims, 1),
+                nn.Conv1d(attn_dims, attn_dims, 1)
+            ]
+            
+            self.attention.append(nn.Sequential(*one_filter))
+        if self.attn > 1:
+            self.attention_resolver = nn.Conv1d(attn_dims * self.attn, attn_dims, 1, 1)
 
         self.resolver = nn.Sequential(
             nn.Conv1d(1, 1, kernel_size=1, bias=True),
-            nn.Tanh()
+            nn.Tanh(),
         )
-        # self.init_weights()
+        
+        self.init_weights()
 
     def init_weights(self):
         for m in self.modules():
@@ -110,41 +119,46 @@ class Autoencoder(nn.Module):
                 nn.init.xavier_normal_(m.weight.data)
 
     def attend(self, x):
-        w = self.attention(x)
-
-        if self.encoder_type == 'SAP':
-            x = x * w
-            # x = torch.sum(x * w, dim=2)
-        elif self.encoder_type == 'ASP':
-            mu = torch.sum(x * w, dim=2)
-            sg = torch.sqrt(
-                (torch.sum((x**2) * w, dim=2) - mu**2).clamp(min=1e-5)
-            )
-            x = torch.cat((mu,sg), 1)
-
-        return x
+        attention_states = []
+        for attention_block in self.attention:
+            f = attention_block[0](x)
+            g = attention_block[1](x)
+            h = attention_block[2](x)
+            combine = nn.Softmax(dim=1)(f * g)
+            attention_states.append(h * combine)
+        attention_states = torch.cat(attention_states, dim=1)
+        
+        if self.attn > 1:
+            return self.attention_resolver(attention_states)
+        else:
+            return attention_states[0]
     
     def resolve_skip_op(self, skip_in, another_skip_in):
         if self.skip_op == 'add':
             return skip_in + another_skip_in
         elif self.skip_op == 'cat':
             return torch.cat((skip_in, another_skip_in), dim=1)
+        elif self.skip_op == 'none':
+            return another_skip_in
 
     def forward(self, x):
         saved = [x]
+        
         for encode in self.encoder:
-            print(x.shape)
             x = encode(x)
             saved.append(x)
-        print(x.shape)
-        x = self.attend(x)
-        for decode in self.decoder:
-            print(x.shape)
+            
+        if self.attn > 0:
+            x = self.attend(x)
+            
+        for index, decode in enumerate(self.decoder):
             encoder_output = saved.pop(-1)
-            layer_in = self.resolve_skip_op(encoder_output, x)
-            #layer_in = torch.cat((encoder_output, x), dim=1)
+            layer_in = encoder_output
+            if index != 0 and self.skip_op == 'cat':
+                layer_in = self.resolve_skip_op(encoder_output, x)
             x = decode(layer_in)
-        print(x.shape)
+            
+
         x = self.resolver(x)
 
         return x
--- test.py
+++ test.py
@@ -12,18 +12,31 @@ def test(model):
     files = os.listdir(f'{TEST_DIR}clean')
     pesq_scores = []
     stoi_scores = []
+    model.eval()
     
     for idx, file in enumerate(files):
         clean_audio, _fs = torchaudio.load(f'{TEST_DIR}/clean/{file}')
         noisy_audio, _fs = torchaudio.load(f'{TEST_DIR}/noisy/{file}')
         noisy_audio = noisy_audio.unsqueeze(0)
-                                      
-        with torch.no_grad():
-            denoised = model(noisy_audio).squeeze().cpu().detach().numpy()
         
-        denoised = np.pad(denoised, [0,1])
-        stoi_scores.append(stoi(clean_audio[0], denoised, fs, extended=False))
-        pesq_scores.append(pesq(clean_audio[0], denoised, fs))
+        clean_audio = clean_audio[:, :32000]
+        noisy_audio = noisy_audio[:, :, :32000]
+        
+        signal_length = clean_audio.shape[-1]
+        
+        if signal_length < 32000:
+            shortage = 32000 - signal_length
+            clean_audio    = torch.nn.functional.pad(clean_audio, (0, shortage))
+            noisy_audio    = torch.nn.functional.pad(noisy_audio, (0, shortage))
+        
+        with torch.no_grad():
+            denoised = model(noisy_audio.cuda()).squeeze().cpu().detach().numpy()
+        
+        clean_for_comparison = clean_audio[0][:signal_length]
+        denoised = denoised[:signal_length]
+
+        stoi_scores.append(stoi(clean_for_comparison, denoised, fs, extended=False))
+        pesq_scores.append(pesq(clean_for_comparison, denoised, fs))
         
         
     pesqs = np.array(pesq_scores)
